dce3fb9c3966d271e639246e75df6c83264914e0
GIS <you@example.com>
2019-08-01 13:15:24 +0700

    Initial Commit

---
+++ Child Care CenterTests/Child_Care_CenterTests.swift
@@ -0,0 +1,34 @@
+//
+//  Child_Care_CenterTests.swift
+//  Child Care CenterTests
+//
+//  Created by GIS on 8/1/19.
+//  Copyright © 2019 GIS. All rights reserved.
+//
+
+import XCTest
+@testable import Child_Care_Center
+
+class Child_Care_CenterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
